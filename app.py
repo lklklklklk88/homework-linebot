f9b0b88dccad532e288f4916630348a3660221f3
lklklklklk88 <juhwang222@gmail.com>
2025-05-17 16:38:21 +0800

    傳送失敗bug修正

--- app.py
+++ app.py
@@ -88,10 +88,6 @@ def remind():
             remind_dt = datetime.datetime.strptime(remind_time, "%H:%M")
             remind_datetime = now.replace(hour=remind_dt.hour, minute=remind_dt.minute, second=0, microsecond=0)
 
-            # 計算現在時間與提醒時間的差距（秒數）
-            remind_dt = datetime.datetime.strptime(remind_time, "%H:%M")
-            remind_datetime = now.replace(hour=remind_dt.hour, minute=remind_dt.minute, second=0, microsecond=0)
-
             # 若提醒時間晚於現在，就跳過
             if now < remind_datetime:
                 continue
